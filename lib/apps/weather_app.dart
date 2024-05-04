d0d1fb1fffd98b41c97c84bd2cc5c21c8ef1323c
GiovaniHaka <haka.giovani@gmail.com>
2024-05-03 21:03:09 -0300

    feat: project structure setup, vscode launch

---
+++ lib/apps/weather_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../modules/home/home_screen.dart';
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: false,
+      ),
+      home: const HomeScreen(
+        title: 'Weather App',
+      ),
+    );
+  }
+}
